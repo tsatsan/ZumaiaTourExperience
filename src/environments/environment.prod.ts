b8eaf61d57d67e18f69f5605cc48bfa1786e15e6
jordiCostaFigueras <desarrolloweb@worktel.com>
2018-07-05 14:49:42 +0200

    changes go back signin

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   MAPBOX_API_KEY: 'pk.eyJ1IjoidHNhdHNhbiIsImEiOiJjamdkZXptbWoyNmgyMzNrbDBqMXZ5djdwIn0.oxleBu3jvaVn-gf_qKbtkw',
-  apiUrl: 'https://historic-lassen-volcanic-42447.herokuapp.com'
+  apiUrl: 'https://historic-lassen-volcanic-42447.herokuapp.com/api/'
 };
